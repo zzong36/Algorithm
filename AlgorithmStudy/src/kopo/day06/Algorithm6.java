b2e3651fac7514572e22aa2fe6612537e13e5283
SEOKYUNG <lastsun4@naver.com>
2022-05-18 10:39:18 +0900

    고고

--- AlgorithmStudy/src/kopo/day06/Algorithm6.java
+++ AlgorithmStudy/src/kopo/day06/Algorithm6.java
@@ -15,7 +15,7 @@ public class Algorithm6 {
 
 		List<Integer> answer = new ArrayList<>();
 
-		// 분해합을 만족하는 수 조합 도출
+		// 분해합을 만족하는 수 조합 도출 6중 for문 사용
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				for (int k = 0; k < 10; k++) {
